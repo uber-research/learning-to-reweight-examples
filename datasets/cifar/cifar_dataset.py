28e4e3a845803c62fdc16672885c44ec6edf4ed2
Mengye Ren <mren3@uber.com>
2019-03-22 11:31:37 -0400

    Fixed license issues.

--- datasets/cifar/cifar_dataset.py
+++ datasets/cifar/cifar_dataset.py
@@ -1,14 +1,5 @@
-# Copyright (c) 2017 - 2019 Uber Technologies, Inc.
-#
-# Licensed under the Uber Non-Commercial License (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at the root directory of this project.
-#
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Modifications Copyright (c) 2019 Uber Technologies, Inc.
 # 
-# Modified from TensorFlow source. License below.
-#
 # Copyright 2016 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- models/cnn/resnet_module.py
+++ models/cnn/resnet_module.py
@@ -1,13 +1,4 @@
-# Copyright (c) 2017 - 2019 Uber Technologies, Inc.
-#
-# Licensed under the Uber Non-Commercial License (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at the root directory of this project.
-#
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Modified from TensorFlow source. License below.
+# Modifications Copyright (c) 2019 Uber Technologies, Inc.
 #
 # Copyright 2016 The TensorFlow Authors. All Rights Reserved.
 #
